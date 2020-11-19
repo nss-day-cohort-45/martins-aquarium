42edd1d2e6c6c3abda7b649ac92e662b27eaf902
Joe Shepherd <joeshepmedia@gmail.com>
2020-11-19 12:38:02 -0600

    chapter 13 updates: list fish by type

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,14 +1,34 @@
-import { useFish } from './FishDataProvider.js'
+import { mostHolyFish, soldierFish, nonHolyFish } from './FishDataProvider.js'
 import { Fish } from './Fish.js'
 
+// List the fish by those types
+// 1) Holy Fish
+// 2) soldiers
+// 3) unworthy
+
 export const FishList = () => {
-  debugger;
 
   // Get a reference to the `<article class="content">` element
   const contentElement = document.querySelector(".fishList")
-  const allTheFish = useFish()
 
-  for (const fishObject of allTheFish) {
+  const holyFish = mostHolyFish()
+  const soldiers = soldierFish()
+  const losers = nonHolyFish()
+
+  // Ugh, this works but is not DRY. How could we reduce the repetition here?
+  for (const fishObject of holyFish) {
+    // console.log("one fish!", fishObject)
+    const fishHTML = Fish(fishObject)
+    // Add to the existing HTML in the content element
+    contentElement.innerHTML += fishHTML
+  }
+  for (const fishObject of soldiers) {
+    // console.log("one fish!", fishObject)
+    const fishHTML = Fish(fishObject)
+    // Add to the existing HTML in the content element
+    contentElement.innerHTML += fishHTML
+  }
+  for (const fishObject of losers) {
     // console.log("one fish!", fishObject)
     const fishHTML = Fish(fishObject)
     // Add to the existing HTML in the content element
